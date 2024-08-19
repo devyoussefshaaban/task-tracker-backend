40fe792d502f38c926a066f2f7afa0abcfef4c39
YOUSSEF <imdevyoussef@gmail.com>
2024-08-19 12:39:49 +0300

    hello text for testing the deploying

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import {config} from 'dotenv'
-import { connectDB } from './configs/dbConfig.js'
-import authRouter from './routes/authRouter.js'
-import tasksRouter from './routes/tasksRouter.js'
-import manageUsersRouter from './routes/admin/manageUsersRouter.js'
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { connectDB } from "./configs/dbConfig.js";
+import authRouter from "./routes/authRouter.js";
+import tasksRouter from "./routes/tasksRouter.js";
+import manageUsersRouter from "./routes/admin/manageUsersRouter.js";
 
-config()
-connectDB()
+config();
+connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/tasks", tasksRouter)
-app.use("/api/v1/manage", manageUsersRouter)
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
-const PORT = process.env.PORT || 4444
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/tasks", tasksRouter);
+app.use("/api/v1/manage", manageUsersRouter);
+
+const PORT = process.env.PORT || 4444;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
