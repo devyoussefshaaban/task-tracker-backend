45cd1d1a3095a837812d907b8a27605aa2a55275
YOUSSEF <imdevyoussef@gmail.com>
2024-08-19 12:27:47 +0300

    admin-portal: getAllUsers & deleteUser EPs added

--- controllers/admin/manageUsersController.js
+++ controllers/admin/manageUsersController.js
@@ -1,20 +1,36 @@
 import User from "../../models/userModel.js";
 import Task from "../../models/taskModel.js";
-import asyncHandler from 'express-async-handler'
+import asyncHandler from "express-async-handler";
+import { USER_ROLE } from "../../utils/constants.js";
 
-export const deleteUser = asyncHandler(async(req, res) => {
-    try {
-        const {params:{userId}} = req
-        
-        const user = await User.findById(userId)
-        
-        if(!user) throw new Error("User not found.")
+export const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find({ role: USER_ROLE.USER });
 
-        await Task.deleteMany({userId})
-        .then(async () => await User.deleteOne({_id: userId}))
-        
-        res.status(201).json({success: true, message: "User has been deleted successfully."})
-    } catch (error) {
-        res.status(400).json({success: false, message: error.message})
-    }
-})
+    res.status(200).json({ success: true, data: users });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+export const deleteUser = asyncHandler(async (req, res) => {
+  try {
+    const {
+      params: { userId },
+    } = req;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found.");
+
+    await Task.deleteMany({ userId }).then(
+      async () => await User.deleteOne({ _id: userId })
+    );
+
+    res
+      .status(201)
+      .json({ success: true, message: "User has been deleted successfully." });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
--- routes/admin/manageUsersRouter.js
+++ routes/admin/manageUsersRouter.js
@@ -1,9 +1,13 @@
-import {Router} from 'express'
-import { deleteUser } from '../../controllers/admin/manageUsersController.js'
-import { admin, auth } from '../../middlewares/authMiddleware.js'
+import { Router } from "express";
+import {
+  deleteUser,
+  getAllUsers,
+} from "../../controllers/admin/manageUsersController.js";
+import { admin, auth } from "../../middlewares/authMiddleware.js";
 
-const router = Router()
+const router = Router();
 
-router.delete("/users/:userId", auth, admin, deleteUser)
+router.get("/users", auth, admin, getAllUsers);
+router.delete("/users/:userId", auth, admin, deleteUser);
 
-export default router
+export default router;
